8a98bb52a0ec22daf0075db0fb35bf3cb19aa8f4
Adam <adamllryan@gmail.com>
2023-09-06 17:38:38 -0400

    change hover menu to push, remove link in title

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,41 +1,40 @@
-
-import './components/Header';
-import './components/Footer';
-import './components/ItemsDisplay';
-import ItemsDisplay from './components/ItemsDisplay';
-import SearchBar from './components/SearchBar';
+import "./components/Header";
+import "./components/Footer";
+import "./components/ItemsDisplay";
+import ItemsDisplay from "./components/ItemsDisplay";
+import SearchBar from "./components/SearchBar";
 import { useState, useEffect, useRef } from "react";
-import Header from './components/Header.jsx'
-import { io } from 'socket.io-client'
+import Header from "./components/Header.jsx";
+import { io } from "socket.io-client";
 
-
-
-const App = ( ) => {
-
-  //TODO: 
+const App = () => {
+  //TODO:
   //Add Panel under search with post, get, update, delete owners
 
   // URL bases
-  let url_items = window.location.href.replace('http://', '').replace('/', '').split(':')
-  let PORT = '5004';
-  let API_URL = url_items[0]
-  
-  if (process.env.NODE_ENV === 'production') {
+  let url_items = window.location.href
+    .replace("http://", "")
+    .replace("/", "")
+    .split(":");
+  let PORT = "5004";
+  let API_URL = url_items[0];
+
+  if (process.env.NODE_ENV === "production") {
     //PORT = window._env_.REACT_APP_PORT
     //API_URL = url_items[0]
-    PORT = url_items[1]
+    PORT = url_items[1];
   }
-  const baseURL = `http://${API_URL}:${PORT}/api/v1`        // Base URL
+  const baseURL = `http://${API_URL}:${PORT}/api/v1`; // Base URL
   //const baseURL = `http://${API_URL}/api/v1`
-  const wsURL = `ws://${API_URL}:${PORT}`
+  const wsURL = `ws://${API_URL}:${PORT}`;
 
   // Data States
 
-  const [items, setItems] = useState([]);               // All items displayed
-  const [more, setMore] = useState(true);               // Are there more items available to load
-  const [page, setPage] = useState(0);                  // Page Number
-  const [owners, setOwners] = useState([])
-  const [search, setSearch] = useState('');             // Search terms in URL string
+  const [items, setItems] = useState([]); // All items displayed
+  const [more, setMore] = useState(true); // Are there more items available to load
+  const [page, setPage] = useState(0); // Page Number
+  const [owners, setOwners] = useState([]);
+  const [search, setSearch] = useState(""); // Search terms in URL string
 
   // Refresh States
 
@@ -45,53 +44,51 @@ const App = ( ) => {
   // Websocket Client setup
 
   const client = useRef();
-  
+
   // Render away from state
 
   useEffect(() => {
-
     // Get owners initially
-    
+
     const getOwners = async () => {
-      let ownersQuery = [{id: 0, name: 'None', password: null}]
+      let ownersQuery = [{ id: 0, name: "None", password: null }];
       try {
-        const res = await fetch(baseURL + '/users')
+        const res = await fetch(baseURL + "/users");
         if (!res.ok) {
-          throw new Error(res.status)
+          throw new Error(res.status);
         }
-        ownersQuery = [...ownersQuery, ...(await res.json())]
+        ownersQuery = [...ownersQuery, ...(await res.json())];
       } catch (err) {
-        console.log(err)
+        console.log(err);
       }
-      setOwners(ownersQuery)
-    }
-    getOwners()
+      setOwners(ownersQuery);
+    };
+    getOwners();
 
     // Get items initially
 
     const getItems = async () => {
-      await newSearch()
-    }
+      await newSearch();
+    };
     getItems();
 
     // Set up websocket the right way
-    const URL = process.env.NODE_ENV === 'prod' ? undefined : wsURL
+    const URL = process.env.NODE_ENV === "prod" ? undefined : wsURL;
     const socket = io(URL);
 
     // Socket on refresh progress update
 
-    socket.on('refresh_progress', (data) => {
-      if (typeof data === 'string' && data==='completed') {
-        setProgress(1)
-        setIsRefreshing(false)
-        setItems([])
-        setPage(0)
-        getNextPage()
+    socket.on("refresh_progress", (data) => {
+      if (typeof data === "string" && data === "completed") {
+        setProgress(1);
+        setIsRefreshing(false);
+        setItems([]);
+        setPage(0);
+        getNextPage();
       } else {
-        setProgress(data)
+        setProgress(data);
       }
-
-    })
+    });
 
     client.current = socket;
 
@@ -99,101 +96,108 @@ const App = ( ) => {
 
     const requestPermission = async () => {
       let permission = await Notification.requestPermission();
-      return permission
-    }
+      return permission;
+    };
     let notificationPermission = requestPermission();
+  }, []);
 
-  }, [])
-  
   // Fetch first 50 results given search param string
 
-  const newSearch = async (params='') => {
+  const newSearch = async (params = "") => {
     try {
-      const res = await fetch(baseURL + '/search/' + params + '&_pgn=0', {
-        method: 'GET',
+      const res = await fetch(baseURL + "/search/" + params + "&_pgn=0", {
+        method: "GET",
         headers: {
-          Accept: 'application/json'
+          Accept: "application/json",
         },
-      })
+      });
       if (!res.ok) {
-        throw new Error(res.status)
+        throw new Error(res.status);
       }
-      setItems([])
-      setItems(await res.json())
-      setSearch(params)
-      setPage(1)
-      setMore(true)
+      setItems([]);
+      setItems(await res.json());
+      setSearch(params);
+      setPage(1);
+      setMore(true);
     } catch (err) {
-      console.log(err)
+      console.log(err);
     }
-  }
+  };
 
   // Get next 50 items on click
 
   const getNextPage = async () => {
     try {
-      const res = await fetch(baseURL + '/search/' + search + '&_pgn=' + page)
+      const res = await fetch(baseURL + "/search/" + search + "&_pgn=" + page);
       if (!res.ok) {
-        throw new Error(res.status)
+        throw new Error(res.status);
       }
-      let json = await res.json()
+      let json = await res.json();
       if (json.length !== 0) {
-        console.log(json)
-        setItems(items => [...items, ...json])
-        setPage(page+1)
+        console.log(json);
+        setItems((items) => [...items, ...json]);
+        setPage(page + 1);
       } else {
-        setMore(false)
+        setMore(false);
       }
     } catch (err) {
-      console.log(err)
+      console.log(err);
     }
-  }
+  };
 
   // Post request to set owner of individual item
 
   const setOwner = async (item_id, owner_id) => {
     try {
-      const res = await fetch(baseURL + '/items/' + item_id, {
-        method: 'POST',
+      const res = await fetch(baseURL + "/items/" + item_id, {
+        method: "POST",
         headers: {
-          'Content-type':'application/json', 
-          Accept: 'application/json'
+          "Content-type": "application/json",
+          Accept: "application/json",
         },
-        body: JSON.stringify({owner_id: owner_id===0?null:owner_id})
-      })
+        body: JSON.stringify({ owner_id: owner_id === 0 ? null : owner_id }),
+      });
       if (!res.ok) {
-        throw new Error(res.status)
+        throw new Error(res.status);
       }
     } catch (err) {
-      console.log(err)
+      console.log(err);
     }
-  }
-  
+  };
+
   // Web socket refresh on click
 
   const refresh = () => {
-    setProgress(0)
-    setIsRefreshing(true)
-    client.current.emit('refresh_page', '')
-  }
-  
+    setProgress(0);
+    setIsRefreshing(true);
+    client.current.emit("refresh_page", "");
+  };
+
   return (
     <>
       <div className="flex flex-col h-screen max-w-4xl m-auto">
-
         {/*Left side panel*/}
-        <div className='group/header z-50'>
-          <Header refreshPage={refresh} progress={progress > 1? .95 : progress} isRefreshing={isRefreshing}/>   
-        <SearchBar submitQuery={newSearch} ownersData={owners}/>
+        <div className="group/header z-50">
+          <Header
+            refreshPage={refresh}
+            progress={progress > 1 ? 0.95 : progress}
+            isRefreshing={isRefreshing}
+            newSearch={newSearch}
+            owners={owners}
+          />
         </div>
         {/*Item display panel*/}
-        <ItemsDisplay page={page} onLoadNext={getNextPage} data={items} setOwner={setOwner} owners={owners} more={more}/>
-
+        <ItemsDisplay
+          page={page}
+          onLoadNext={getNextPage}
+          data={items}
+          setOwner={setOwner}
+          owners={owners}
+          more={more}
+        />
       </div>
-
-      
     </>
   );
-}
+};
 
 export default App;
--- client/src/components/SearchBar.jsx
+++ client/src/components/SearchBar.jsx
@@ -1,50 +1,48 @@
-import React from 'react'
-import { useState } from 'react'
-import { TagsInput } from 'react-tag-input-component';
-import './TagsInput.css'
-import OwnerDropdown from './OwnerDropdown';
+import React from "react";
+import { useState } from "react";
+import { TagsInput } from "react-tag-input-component";
+import "./TagsInput.css";
+import OwnerDropdown from "./OwnerDropdown";
+import { Menu, MenuItem } from "@material-tailwind/react";
 
 const SearchBar = ({ submitQuery, ownersData }) => {
-
   // App states
 
-  const [names, setNames] = useState([])        // Name tags selected
-  const [auctions, setAuctions] = useState([])  // Auction tags selected
-  const [conditions, setConditions] = useState([]) // Item Conditions selected
-  const [ownerId, setOwnerId] = useState(0)      // Owner tags selected
-  
+  const [names, setNames] = useState([]); // Name tags selected
+  const [auctions, setAuctions] = useState([]); // Auction tags selected
+  const [conditions, setConditions] = useState([]); // Item Conditions selected
+  const [ownerId, setOwnerId] = useState(0); // Owner tags selected
+
   // Format search query string
 
   const onSubmit = () => {
-
     // Format key with values arrow function
 
     const formatQuery = (title, list) => {
-      return title + '=' + list.join('%25')
-    }
+      return title + "=" + list.join("%25");
+    };
 
-    let result = []
+    let result = [];
 
     // Format name
 
-    if (names.length>0) {
-      result.push(formatQuery('name', names))
+    if (names.length > 0) {
+      result.push(formatQuery("name", names));
     }
 
     // Format Auctions
 
-    if (auctions.length>0) {
-      result.push(formatQuery('auction', auctions))
+    if (auctions.length > 0) {
+      result.push(formatQuery("auction", auctions));
     }
 
-    if (conditions.length>0) {
-      result.push(formatQuery('condition', conditions))
+    if (conditions.length > 0) {
+      result.push(formatQuery("condition", conditions));
     }
 
     // Format Owners
 
     if (ownerId !== 0) {
-
       // Remove non-matches
 
       //let filtered = ownersData.filter((owner) => {return owners.indexOf(owner.name) !== -1})
@@ -54,34 +52,71 @@ const SearchBar = ({ submitQuery, ownersData }) => {
       //filtered = filtered.map((owner) => owner.id)
 
       //result.push(formatQuery('owner_id', filtered))
-      result.push('owner_id=' + ownerId)
+      result.push("owner_id=" + ownerId);
     }
-    
-    // Format for url 
 
-    result = result.join('&').replaceAll(' ', '+')
+    // Format for url
 
-    submitQuery(result)
-  }
-  
+    result = result.join("&").replaceAll(" ", "+");
+
+    submitQuery(result);
+  };
+
   return (
-    <div className=' left-1/3 right-1/3 rounded-md border border-slate-400 -top-1/2 group-hover/header:top-20 absolute p-4 shadow-xl duration-300 ease-in-out z-30 bg-slate-50'>
+    <>
+      {/* Search Terms*/}
 
-        {/* Search Terms*/}
+      <MenuItem color="lightBlue">
+        <TagsInput
+          className="rounded-none"
+          value={names}
+          onChange={setNames}
+          name="names"
+          placeHolder="Add Keywords e.g. Power Strip"
+        />
+      </MenuItem>
 
-        <TagsInput className='rounded-none' value={names} onChange={setNames} name='names' placeHolder='Add Keywords e.g. Power Strip' />
-        <TagsInput className='' value={auctions} onChange={setAuctions} name='auctions' placeHolder='Add Auction keywords e.g. Stow or 3010' />
-        <TagsInput className='' value={conditions} onChange={setConditions} name='conditions' placeHolder='Add Condition Keywords e.g. New' />
-        <div className='inline-flex items-center gap-x-2 m-2 border border-slate-400 rounded-md p-1 bg-slate-50'>
+      <MenuItem color="lightBlue">
+        <TagsInput
+          className=""
+          value={auctions}
+          onChange={setAuctions}
+          name="auctions"
+          placeHolder="Add Auction keywords e.g. Stow or 3010"
+        />
+      </MenuItem>
+
+      <MenuItem color="lightBlue">
+        <TagsInput
+          className=""
+          value={conditions}
+          onChange={setConditions}
+          name="conditions"
+          placeHolder="Add Condition Keywords e.g. New"
+        />
+      </MenuItem>
+      <MenuItem color="lightBlue">
+        <div className="inline-flex items-center gap-x-2 m-2 border border-slate-400 rounded-md p-1 bg-slate-50">
           <label>Filter by Owner: </label>
-          <OwnerDropdown owners={ownersData} owner_id={ownerId} updateOwner={setOwnerId}/>
+          <OwnerDropdown
+            owners={ownersData}
+            owner_id={ownerId}
+            updateOwner={setOwnerId}
+          />
         </div>
-        {/* Submit Button */}
-
-        <button type="button" className="w-full text-white bg-gray-800 hover:bg-gray-900 focus:outline-none focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-gray-800 dark:hover:bg-gray-700 dark:focus:ring-gray-700 dark:border-gray-700" onClick={onSubmit}>Apply</button>
-        
-    </div>
+      </MenuItem>
+      {/* Submit Button */}
+      <MenuItem color="lightBlue">
+        <button
+          type="button"
+          className="w-full text-white bg-gray-800 hover:bg-gray-900 focus:outline-none focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-gray-800 dark:hover:bg-gray-700 dark:focus:ring-gray-700 dark:border-gray-700"
+          onClick={onSubmit}
+        >
+          Apply
+        </button>
+      </MenuItem>
+    </>
   );
-}
+};
 
-export default SearchBar
+export default SearchBar;
