169e2df54121cdb99f1c9be66c4c6ca07e9b5828
Adam <adamllryan@gmail.com>
2023-09-08 15:00:52 -0400

    refactor header content for readability

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -9,72 +9,99 @@ import {
   MenuItem,
 } from "@material-tailwind/react";
 import SearchBar from "./SearchBar";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faChevronDown, faChevronUp } from "@fortawesome/free-solid-svg-icons";
+
+/* Title Component */
+
+const HeaderTitle = () => {
+  return (
+    <Typography variant="h6" className="text-black py-1.5 z-10">
+      Online Liquidation Auction Handler
+    </Typography>
+  );
+};
+
+/* Search Button Component */
+
+const SearchButton = ({ newSearch, owners }) => {
+  return (
+    <div className="text-black  left-1/2 z-0 text-center">
+      <Menu placement="bottom" dismiss={{ itemPress: false }}>
+        <MenuHandler>
+          <Button>Search</Button>
+        </MenuHandler>
+        <MenuList className="z-50">
+          <SearchBar submitQuery={newSearch} ownersData={owners} />
+        </MenuList>
+      </Menu>
+    </div>
+  );
+};
+
+/* Refresh Button Component */
+
+const RefreshButton = ({ refreshPage, isRefreshing, progress }) => {
+  return (
+    <div className="">
+      <Button
+        variant="gradient"
+        size="sm"
+        className="inline-block z-10"
+        onClick={refreshPage}
+        disabled={isRefreshing}
+      >
+        <span className="text-black">
+          {isRefreshing ? (
+            <div
+              className="inline-flex items-center justify-center content-center"
+              role="status"
+            >
+              <svg
+                aria-hidden="true"
+                className="w-4 h-4 mr-2 text-gray-200 animate-spin dark:text-gray-600 fill-blue-600"
+                viewBox="0 0 100 101"
+                fill="none"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path
+                  d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z"
+                  fill="currentColor"
+                />
+                <path
+                  d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z"
+                  fill="currentFill"
+                />
+              </svg>
+              <span className="sr-only">Loading...</span>
+              <label>{(progress * 100).toFixed(2) + "%"}</label>
+            </div>
+          ) : (
+            "Refresh Items"
+          )}
+        </span>
+      </Button>
+    </div>
+  );
+};
+
+/* Main Header Component */
+
 const Header = ({ refreshPage, progress, isRefreshing, newSearch, owners }) => {
   return (
     <Navbar className="relative w-full p-4 pb-0 z-50 border-0 rounded-none">
       <div className="container inline-flex justify-between items-center text-blue-gray-900 ">
         {/* Title */}
-
-        <Typography variant="h6" className="text-black py-1.5 z-10">
-          Online Liquidation Auction Handler
-        </Typography>
+        <HeaderTitle />
         <div className="inline-flex gap-x-2 border border-slate-400 shadow-xl">
           {/* Toggle Search Button */}
 
-          <div className="text-black  left-1/2 z-0 text-center">
-            <Menu placement="bottom" dismiss={{ itemPress: false }}>
-              <MenuHandler>
-                <Button>Search</Button>
-              </MenuHandler>
-              <MenuList className="z-50">
-                <SearchBar submitQuery={newSearch} ownersData={owners} />
-              </MenuList>
-            </Menu>
-          </div>
-
+          <SearchButton newSearch={newSearch} owners={owners} />
           {/* Refresh + Refresh Status Button */}
 
-          <div className="">
-            <Button
-              variant="gradient"
-              size="sm"
-              className="inline-block z-10"
-              onClick={refreshPage}
-              disabled={isRefreshing}
-            >
-              <span className="text-black">
-                {isRefreshing ? (
-                  <div
-                    className="inline-flex items-center justify-center content-center"
-                    role="status"
-                  >
-                    <svg
-                      aria-hidden="true"
-                      className="w-4 h-4 mr-2 text-gray-200 animate-spin dark:text-gray-600 fill-blue-600"
-                      viewBox="0 0 100 101"
-                      fill="none"
-                      xmlns="http://www.w3.org/2000/svg"
-                    >
-                      <path
-                        d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z"
-                        fill="currentColor"
-                      />
-                      <path
-                        d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z"
-                        fill="currentFill"
-                      />
-                    </svg>
-                    <span className="sr-only">Loading...</span>
-                    <label>{(progress * 100).toFixed(2) + "%"}</label>
-                  </div>
-                ) : (
-                  "Refresh Items"
-                )}
-              </span>
-            </Button>
-          </div>
+          <RefreshButton
+            refreshPage={refreshPage}
+            progress={progress}
+            isRefreshing={isRefreshing}
+          />
         </div>
       </div>
     </Navbar>
